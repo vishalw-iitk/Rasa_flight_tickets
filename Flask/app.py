4621a2fc1d2f66814f51e84dcc593ba147a99820
vishalw-iitk <vishalwghmre88@gmail.com>
2022-09-06 16:25:14 +0530

    added flights_info route and checking after long with pending code push

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +8,12 @@ CORS(app)
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     return render_template('home.html')
+
+@app.route('/flights_info', methods = ['POST'])
+def flights_information():
+    request.args.get('returenDate')
+    request.args['departureDate']
+    return amadeus.run()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
